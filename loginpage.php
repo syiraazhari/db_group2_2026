7dc4b67af6fdc19f90081db0b579454bad14752d
GOH WEN ZE <wzgoh070205@gmail.com>
2026-06-11 09:29:04 +0800

    Revert "update login page"
    This reverts commit 77e855d9d59681224b89801088edf4c266fb664e.

--- loginpage.php
+++ loginpage.php
@@ -2,21 +2,23 @@
 session_start();
 include 'connect_db.php';
 
-if(isset($_POST['signInButton'])) {
-    $loginName = $_POST['loginName'];
-    $loginPassword = $_POST['loginPassword'];
-    
-    $sql = "SELECT * FROM customer WHERE Username = '$loginName' OR Email = '$loginName'";
-    $result = mysqli_query($conn, $sql);
-    
-    if($result -> num_rows > 0) {
+if(isset($_POST['signInButton'])){
+  $loginName = $_POST['loginName'];
+  $loginPassword = $_POST['loginPassword'];
+
+  $sql = "SELECT * FROM CUSTOMER
+          WHERE Username = $loginName OR Email = $loginName";
+  $result = mysqli_query($conn, $sql);
+
+  if($result -> num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
-        if($loginPassword == $row['Password']) {
-            $_SESSION['user_id'] = $row['CustomerID'];
-            $_SESSION['username'] = $row['Username'];
+        if(password_verify($loginPassword, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['username'] = $row['username'];
             header("Location: home.php");
             exit();
-        } else {
+        } 
+        else {
             $error = "Invalid password";
         }
     } else {
@@ -29,29 +31,42 @@ if(isset($_POST['signInButton'])) {
 <html>
 <head>
   <title>Login Page</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" 
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" >
 
-  <style>
+<style>
     body {
         background-color: #72c698;
         font-family: Verdana;
     }
-  </style>
+</style>
 </head>
 
 <body>
-<h1 style="margin:20px;">Login to Your Account</h1>
+<h1 style = "margin: 20px;"><b>Login to Your Account</b></h1>
+<hr style = "width: 2px;">
 
-<div style="margin:20px;">
-  <form method="POST">
-    <input type="text" name="loginName" 
-    class="form-control w-50 mb-3" placeholder="Username or Email" required><br>
-    <input type="password" name="loginPassword" 
-    class="form-control w-50 mb-3" placeholder="Password" required><br>
-    <input type="submit" name="signInButton" value="Sign in" class="btn btn-primary">
-    <p class="mt-3">Not a member? <a href="register.php">Register</a></p>
-  </form>
+<div class="tab-content" style = "margin: 20px;">
+  <div class="tab-pane fade show active">
+    <form action = "" method = "POST">
+      <div data-mdb-input-init class="col-md-6 mb-4">
+        <label>Username or Email</label>
+        <input type="text" name="loginName" class = "form-control " required><br>
+      </div>
+
+      <div data-mdb-input-init class="col-md-6 mb-4">
+        <label>Password</label>
+        <input type="password" name="loginPassword" class = "form-control" required><br>
+      </div>
+
+      <div data-mdb-input-init class="col-md-6 mb-4">
+      <input type="submit" data-mdb-button-init data-mdb-ripple-init 
+      name = "signInButton" value = "Sign in" class="btn btn-primary btn-block mb-4">
+      </div>
+
+        <p>Not a member? <a href="register.php">Register</a></p>
+    </form>
+  </div>
 </div>
-
 </body>
 </html>
